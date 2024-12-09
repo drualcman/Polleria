127ecfd9686ff3380aca5f07405730bf58246a39
Sergi Ortiz Gomez <drualcman@gmail.com>
2024-12-09 14:45:08 +0800

    Update code to NET 9

---
+++ BlazingPolleria/Model/OrderWithStatus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazingPizza
+{
+    public class OrderWithStatus
+    {
+        public readonly static TimeSpan PreparationDuration = TimeSpan.FromSeconds(10);
+        public readonly static TimeSpan DeliveryDuration = TimeSpan.FromMinutes(1); // Irreal, pero más interesante de ver
+
+        public Order Order { get; set; }
+
+        public string StatusText { get; set; }
+
+        public bool IsDelivered => StatusText == "Delivered";
+
+        public static OrderWithStatus FromOrder(Order order)
+        {
+            // Para simular un proceso de backend real, falsificamos actualizaciones de estado 
+            // según la cantidad de tiempo desde que se realizó el pedido
+            string statusText;
+            var dispatchTime = order.CreatedTime.Add(PreparationDuration);
+
+            if (DateTime.Now < dispatchTime)
+            {
+                statusText = "Preparando";
+            }
+            else if (DateTime.Now < dispatchTime + DeliveryDuration)
+            {
+                statusText = "Enviado para entrega";
+            }
+            else
+            {
+                statusText = "Entregado";
+            }
+
+            return new OrderWithStatus
+            {
+                Order = order,
+                StatusText = statusText
+            };
+        }
+
+
+    }
+}
---
+++ BlazingPolleria/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// wwwroot/js/site.js
+window.setTheme = (theme) => {
+    let linkElement = document.querySelector("link#theme-css");
+
+    if (!linkElement) {
+        linkElement = document.createElement("link");
+        linkElement.id = "theme-css";
+        linkElement.rel = "stylesheet";
+        document.head.appendChild(linkElement);
+    }
+
+    linkElement.href = `css/theme-${theme}.css`;
+};
+
+
